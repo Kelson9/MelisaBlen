e75ebd8cb49a97a34a4d5cb0228940f66ad37b45
Kelson9 <kelsonngoe@gmail.com>
2021-05-10 02:02:01 -1100

    hosting

--- src/app/examples/landing/landing.component.ts
+++ src/app/examples/landing/landing.component.ts
@@ -17,7 +17,7 @@ export class LandingComponent implements OnInit {
   ngOnInit() {
     const navbar: HTMLElement = this.element.nativeElement;
     this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
-}
+} 
 sidebarOpen() {
     const toggleButton = this.toggleButton;
     const html = document.getElementsByTagName('html')[0];
